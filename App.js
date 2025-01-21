0779289b94d67d29c31bcffd73eb233fe0a97950
pannicking <23011867@myrp.edu.sg>
2025-01-21 10:39:10 +0800

    App.js

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState,useEffect} from 'react';
+import { FlatList, StatusBar, Text, TextInput, View} from 'react-native';
 
-export default function App() {
+let originalData=[];
+
+const App = () => {
+  const [mydata, setMyData] = useState([]);
+
+  useEffect(( )=>{
+    //Add fetch()- exercise 1a
+    fetch("https://jsonplaceholder.typicode.com/albums")
+        .then((response)=>{
+            return response.json();
+        })
+        .then((myJson)=>{
+           if(originalData.length < 1)
+           {
+               setMyData(myJson)
+               originalData=myJson;
+
+           }
+        })
+},[]);
+
+  //create filterdata() function
+    const FilterData=(text)=>{
+        if(text!=''){
+            let myFilteredData= originalData.filter((item)=>
+            item.title.includes(text));
+            setMyData(myFilteredData);
+        }
+        else{
+            setMyData(originalData);
+        }
+    }
+
+
+  const renderItem = ({item, index}) => {
+    return (
+    <View>
+    <Text style={{borderWidth:1}}>{item.title}</Text>
+    </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar/>
+      <Text>Search:</Text>
+      <TextInput style={{borderWidth:1}} onChangeText={(text)=>{FilterData(text)}} />>
+      <FlatList data={mydata} renderItem={renderItem} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
